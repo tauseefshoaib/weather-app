af288c1747fd91f6b62502d3e789bc6e03f189fc
tauseefshoaib <tauseefshoaib@gmail.com>
2024-01-16 15:09:50 +0530

    responsive fixes

--- src/App.jsx
+++ src/App.jsx
@@ -33,44 +33,46 @@ function App() {
         value={location}
         onKeyDown={searchWeather}
         onChange={onChangeInput}
-        className="p-4 bg-slate-200 rounded-3xl h-10 w-[350px] text-slate-950  outline-none"
+        className="p-4 bg-slate-200 rounded-3xl h-10 w-[90%] md:w-[50%] text-slate-950  outline-none"
       />
       {data === undefined || data.cod === "404" ? (
         <h1>{errorText}</h1>
       ) : (
         <>
-          <div className="flex flex-col justify-between items-center h-[70%]">
+          <div className="flex flex-col justify-between items-center h-[80%]">
             <div className="flex flex-col justify-between items-center">
-              <h1>{`${data?.name}, ${getCountryName(data?.sys?.country)}`}</h1>
-              <h1 className="text-9xl md:text-5xl mt-[100px] md:mt-[50px] ">
+              <h1 className="text-xl md:text-3xl">{`${
+                data?.name
+              }, ${getCountryName(data?.sys?.country)}`}</h1>
+              <h1 className=" text-8xl md:text-9xl mt-[150px] mb-6 md:mt-[150px]">
                 {`${data?.main?.temp?.toFixed(1)} °C`}
               </h1>
               <div className="flex flex-row justify-between items-center">
-                <h1 className="py-5 px-10 text-3xl md:text-md">{`${data?.main?.temp_min?.toFixed(
+                <h1 className="py-5 px-10 text-xl md:text-3xl">{`⬇ ${data?.main?.temp_min?.toFixed(
                   1
                 )} °C`}</h1>
-                <h1 className="py-5 px-10 text-3xl md:text-md">{`${data?.main?.temp_max?.toFixed(
+                <h1 className="py-5 px-10 text-xl md:text-3xl">{`⬆ ${data?.main?.temp_max?.toFixed(
                   1
                 )} °C`}</h1>
               </div>
             </div>
           </div>
-          <div className="flex flex-row justify-evenly items-center px-4 py-2 my-2 rounded-2xl bg-slate-400 w-[70%] md:w-[90%]">
+          <div className="flex flex-row justify-evenly items-center px-4 py-2 my-2 rounded-2xl bg-slate-400 w-[90%] md:w-[50%]">
             <div className="flex flex-col justify-between items-center ">
-              <h1 className="text-2xl md:text-md">{`${data?.main?.feels_like?.toFixed(
+              <h1 className="text-md md:text-2xl">{`${data?.main?.feels_like?.toFixed(
                 1
               )} °C`}</h1>
-              <h1 className="text-2xl md:text-md">Feels Like</h1>
+              <h1 className="text-md md:text-2xl">Feels Like</h1>
             </div>
             <div className="flex flex-col justify-between items-center ">
-              <h1 className="text-2xl md:text-md">{`${data?.main?.humidity} %`}</h1>
-              <h1 className="text-2xl md:text-md">Humidity</h1>
+              <h1 className="text-md md:text-2xl">{`${data?.main?.humidity} %`}</h1>
+              <h1 className="text-md md:text-2xl">Humidity</h1>
             </div>
             <div className="flex flex-col justify-between items-center ">
-              <h1 className="text-2xl md:text-md">{`${convertMpsToKph(
+              <h1 className="text-md md:text-2xl">{`${convertMpsToKph(
                 data?.wind?.speed
               )?.toFixed(1)} km/h`}</h1>
-              <h1 className="text-2xl md:text-md">Wind</h1>
+              <h1 className="text-md md:text-2xl">Wind</h1>
             </div>
           </div>
         </>
